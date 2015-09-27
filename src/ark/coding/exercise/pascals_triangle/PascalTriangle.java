673fd4c6c14b3ff3b48401b235fb02805198a246
akshayrajkore <akshayrajkore@gmail.com>
2015-09-26 22:28:20 -0400

    ARK : print PascalTriangle by taking triangle height as a parameter

--- src/ark/coding/exercise/pascals_triangle/PascalTriangle.java
+++ src/ark/coding/exercise/pascals_triangle/PascalTriangle.java
@@ -6,18 +6,38 @@ package ark.coding.exercise.pascals_triangle;
 public class PascalTriangle {
 
     public static void main(String[] args){
-        PascalTriangle.triangle(5);
+        int triangle_height = 1;
+        PascalTriangle.constructPascalTriangle(triangle_height);
+        PascalTriangle.getRow(triangle_height);
     }
 
-    private static int[] triangle(int row_no) {
+    private static void constructPascalTriangle(int rows){
+        for(int row_no = 1; row_no <= rows; row_no++) {
+            getRow(rows,row_no);
+            System.out.println();
+        }
+    }
+
+    private static void getRow(int rows, int rowToReturn){
+        int row_no = rowToReturn;
+        int element = 1;
+        System.out.format("%" + (rows-row_no+1)*2 + "s","");
+        for(int element_no = 1; element_no <= row_no; element_no++) {
+            System.out.format("%4d",element);
+            element = element * (row_no - element_no) / (element_no);
+        }
+    }
+    /*
+       Gets the elements at the specified row in a Pascal's triangle
+     */
+    private static int[] getRow(int rowToReturn) {
+        int row_no = rowToReturn;
         int element = 1;
         int[] row = new int[row_no];
-        row[0] = element;
-        System.out.println("element: " + row[0]);
-        for(int i = 1 ; i < row_no ; i++ ){
-            element = element * (row_no - i)/i;
-            row[i] = element;
-            System.out.println("element: " + row[i]);
+        for(int element_no = 1; element_no <= row_no; element_no++) {
+            row[element_no - 1] = element;
+            System.out.format("%4d",element);
+            element = element * (row_no - element_no) / (element_no);
         }
         return row;
     }
