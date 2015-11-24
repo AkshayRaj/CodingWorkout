7379dd5e5f62423b5cd5ff591485ad299c3cb2e1
akshayrajkore <akshayrajkore@gmail.com>
2015-11-24 01:55:41 -0500

    ARK: made code better for FrogCrossingAlgorithm

--- src/ark/coding/brainteaser/frogcrossingalgorithm/FrogLeap.java
+++ src/ark/coding/brainteaser/frogcrossingalgorithm/FrogLeap.java
@@ -2,17 +2,16 @@ package ark.coding.brainteaser.frogcrossingalgorithm;
 
 import ark.coding.Solution;
 
-/**
- * Created by Akshayraj on 11/14/15.
- */
 public class FrogLeap implements Solution<Integer>{
     private static final int CANNOT_REACH_POSITION_X = -1;
     private static int[] mLeafPositions = {
            5,
+           1,
            2
     };
     private static int mDestination = 8;
     private static int mJumpCapacity = 3;
+
     @Override
     public Integer solution(Object... args) {
         return getEarliestTime((int[]) args[0],(int) args[1],(int) args[2]);
@@ -22,29 +21,27 @@ public class FrogLeap implements Solution<Integer>{
         int lastLeafTime = A.length - 1;//time last leaf fell is length of array - 1, as array-index is 0-based.
         int leafBitMap[] = new int[X+1];
         int positionLeafFell = 0;
-        leafBitMap[0] = 1;//at position 0, we assume "leaf is present"
-
+        leafBitMap[0] = 1;//at position 0, we assume "leaf is present"\
         for(int time = 0; time <= lastLeafTime; time++){
             positionLeafFell = A[time];
             leafBitMap[positionLeafFell] = 1;
             System.out.println("positionLeafFell: " + positionLeafFell);
-            //check if frog can travel from position '0' to position 'X'
+            //check if frog can travel from position '0' to position 'X', after a new leaf has fallen
+            // We require two "pointers" :
+            // -one tracks currentPosition
+            // -the other checks jumps possible from currentPosition
             int currentPosition = 0;
-            if(leafBitMap[currentPosition] == 1) {
-                int possibleJumpPosition = currentPosition+1;
-                do{
-                    if(possibleJumpPosition <= X) {
-                        if (leafBitMap[possibleJumpPosition] == 1) {
-                            currentPosition = possibleJumpPosition;//frog hops if leaf present within D from currentPosition
-                        }
-                        possibleJumpPosition++;
+            int possibleJumpPosition = currentPosition+1;
+            while(possibleJumpPosition <= currentPosition + D && possibleJumpPosition <= X){
+                if(possibleJumpPosition <= X) {
+                    if (leafBitMap[possibleJumpPosition] == 1) {
+                        currentPosition = possibleJumpPosition;//frog hops, if possible, from currentPosition
                     }
-                }while(possibleJumpPosition <= currentPosition + D &&
-                        possibleJumpPosition <= X);//this ensures exit from while loop, when possibleJumpPosition == X
-                possibleJumpPosition--;
-                if(possibleJumpPosition == X){
-                    return time;
+                    possibleJumpPosition++;
                 }
+            }
+            if(possibleJumpPosition >= X){
+                return time;
             }
         }
         return CANNOT_REACH_POSITION_X;
@@ -54,6 +51,4 @@ public class FrogLeap implements Solution<Integer>{
         int timeFrogJumps = new FrogLeap().solution(mLeafPositions, mDestination, mJumpCapacity);
         System.out.println("EarliestTimeFrogJumps: " + timeFrogJumps);
     }
-
-
 }
