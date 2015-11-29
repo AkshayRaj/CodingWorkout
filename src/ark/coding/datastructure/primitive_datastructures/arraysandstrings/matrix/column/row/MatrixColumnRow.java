46c09d08dc9a6c4dbb39923c432d4141edb5f5bf
akshayrajkore <akshayrajkore@gmail.com>
2015-11-28 19:25:52 -0500

    ARK: Replace matrix[row][column] with Zeros using first row and first column as temp storage. Time-O(N^2); Space-O(1)

--- src/ark/coding/datastructure/primitive_datastructures/arraysandstrings/matrix/column/row/MatrixColumnRow.java
+++ src/ark/coding/datastructure/primitive_datastructures/arraysandstrings/matrix/column/row/MatrixColumnRow.java
@@ -21,22 +21,20 @@ public class MatrixColumnRow implements Solution<int[][]> {
     public int[][] replaceRowColumnWithZero(int[][] matrix) {
         int rows = matrix.length;//number of rows is the height of the matrix
         int columns = matrix[0].length;//number of columns is length of the first element(assuming all rows have equal length)
-        int[] rowNoForZeros = new int[rows];
-        int[] columnNoForZeros = new int[columns];
         //find zeros in the matrix
         for(int row = 0; row < rows; row++){
             System.out.print("\n");
             for(int column = 0 ; column < columns; column++){
                 System.out.print(" " + matrix[row][column]);
                 if(matrix[row][column] == 0){
-                    rowNoForZeros[row] = 1;
-                    columnNoForZeros[column] = 1;
+                    matrix[row][0] = 0;
+                    matrix[0][column] = 0;
                 }
             }
         }
         //make the row Zero
         for(int row = 0; row < rows; row++){
-            if(rowNoForZeros[row] == 1){
+            if(matrix[row][0] == 0){
                 for(int column = 0; column < columns; column++){
                     matrix[row][column] = 0;
                 }
@@ -44,7 +42,7 @@ public class MatrixColumnRow implements Solution<int[][]> {
         }
         //make the column Zero
         for(int column = 0; column < columns; column++){
-            if(columnNoForZeros[column] == 1){
+            if(matrix[0][column] == 0){
                 for(int row = 0; row < rows; row++){
                     matrix[row][column] = 0;
                 }
