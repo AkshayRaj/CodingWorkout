49feee5e41f18df730bbb9289830868d8052b221
akshayrajkore <akshayrajkore@gmail.com>
2016-02-19 23:33:03 -0500

    ARK: Using vars to check true and false cases 	modified:   src/ark/coding/exercise/number_in_intervals/Intervals.java

--- src/ark/coding/exercise/number_in_intervals/Intervals.java
+++ src/ark/coding/exercise/number_in_intervals/Intervals.java
@@ -21,8 +21,10 @@ public class Intervals {
 
     public static void main(String[] args){
         Intervals intervals = new Intervals();
-        System.out.println("is 60 in interval: " + intervals.isNumberInInterval(45));
-        System.out.print("is 38 in interval: " + intervals.isNumberInInterval(38));
+        long numberInInterval = 38;//use this to test assertTrue
+        long numberNotInInterval = 45;//use this to test assertFalse
+        System.out.println("is " + numberInInterval + "in interval: " + intervals.isNumberInInterval(numberInInterval));
+        System.out.print("is" + numberNotInInterval + "in interval: " + intervals.isNumberInInterval(numberNotInInterval));
     }
 
     public boolean isNumberInInterval(long number){
