eb8c49ccfac24b89f2281182c17165d3a032023c
akshayrajkore <akshayrajkore@gmail.com>
2019-09-16 23:54:18 -0400

    Memoize the RodCutter problem

--- src/main/java/ark/coding/algorithm/dynamic_programming/cutting_a_rod/RodCutter.java
+++ src/main/java/ark/coding/algorithm/dynamic_programming/cutting_a_rod/RodCutter.java
@@ -9,11 +9,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RodCutter implements Solution<Integer> {
-
+    private static int[] maxCostComputations;
     public static void main(String[] args) {
         RodCutter rodCutter = new RodCutter();
         List<Integer> prices = Arrays.asList(new Integer[] {1, 5, 8, 9, 10, 17, 17, 20});
         int length = prices.size();
+        maxCostComputations = rodCutter.getMaxCostStore(length);
         System.out.println("Max cost that can be extracted is: " + rodCutter.solution(prices, length));
     }
 
@@ -30,21 +31,31 @@ public class RodCutter implements Solution<Integer> {
             // Recursive methods should always have a halting condition to avoid Stack Overflow.
             return 0; // cost of a rod with zero or negative length is zero (does not exist.)
         }
+        int maxCost = maxCostComputations[rodLength-1];
 
-        int maxCost = -1;
-        for (int lengthOfCut = 1; lengthOfCut <= rodLength; lengthOfCut++) {
-            int lengthOfRemainingCut = rodLength - lengthOfCut;
-            maxCost = Math.max(
-                    maxCost,
-                    getPriceOfCut(prices, lengthOfCut) + maxCostOfRod(prices, lengthOfRemainingCut));
-            System.out.println("Max cost for rod of length " + rodLength + ","
-                    + " is " + maxCost);
+        // if cost is not already computed, then do it now.
+        if (maxCost == Integer.MIN_VALUE) {
+            System.out.println("Length of rod: " + rodLength);
+            for (int lengthOfCut = 1; lengthOfCut <= rodLength; lengthOfCut++) {
+                int lengthOfRemainingCut = rodLength - lengthOfCut;
+                maxCost = Math.max(
+                        maxCost,
+                        getPriceOfCut(prices, lengthOfCut) + maxCostOfRod(prices, lengthOfRemainingCut));
+            }
+            // store the computation for later use. (memoization)
+            maxCostComputations[rodLength-1] = maxCost;
         }
         return maxCost;
     }
 
     private Integer getPriceOfCut(final List<Integer> prices, final int cutLength) {
         return prices.get(cutLength - 1);
+    }
+
+    private int[] getMaxCostStore(int length) {
+        int[] storeForMaxCost = new int[length];
+        Arrays.fill(storeForMaxCost, Integer.MIN_VALUE);
+        return storeForMaxCost;
     }
 
     @Override
