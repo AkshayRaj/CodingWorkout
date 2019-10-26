daa985bcc2072b79525b4500492d51b0148ce6e1
akshayrajkore <akshayrajkore@gmail.com>
2019-10-25 23:17:31 -0400

    DataStructures: Listed operations for MinHeap. - Heapify is the main function that needs to be studied & implemented.

--- src/main/java/ark/coding/datastructure/primitive_datastructures/graph/MinHeap.java
+++ src/main/java/ark/coding/datastructure/primitive_datastructures/graph/MinHeap.java
@@ -36,7 +36,7 @@ public class MinHeap {
      */
     int[] heap = new int[]{};
 
-    public int[] build(final int[] array) {
+    public int[] buildHeap(final int[] array) {
         // array representing the heap will have empty cells,
         // if the number of nodes in heap are not exactly equal to 2 raised to some power, i.e (2^power)
         int heapArrayLength = array.length;
@@ -48,10 +48,89 @@ public class MinHeap {
         heap = Arrays.copyOf(array, heapArrayLength);
         Arrays.fill(heap, array.length, heapArrayLength-1, Integer.MAX_VALUE);
 
-        for (int index = (heap.length/2); index >= 0; index--) {
-
+        for (int index = 0; index < array.length; index++) {
+            heapify(heap, array[index]);
         }
         return heap;
+    }
+
+    private void heapify(final int[] heap, int index) {
+        // TODO: Implement the heapify algorithm.
+    }
+
+    public int insert(final int element) {
+        int elementKey = -1;
+        // TODO: Insert the element, and re-balance the heap
+
+        return elementKey;
+    }
+
+    /**
+     * Decreases value of the element at index {@code elementKey} to {@code newValue}
+     *
+     * @param elementKey key of the element whose value is to be decreased.
+     * @param newValue the new value of the element
+     * @return the new key (array index) of the element.
+     */
+    public int decreaseKey(final int elementKey, final int newValue) {
+        int newKey = -1;
+        // TODO: Decreases value of element to newValue
+
+        return newKey;
+    }
+
+    public int[] delete(final int elementKey) {
+        // TODO: re-balance the heap after deleting the element from the heap.
+
+        return heap;
+    }
+
+    /**
+     * Pop the root, and push the new element.
+     * Re-balances the heap after pop & push.
+     *
+     * @param elementValue the element to replace the root.
+     *                Note that, the new element may not become the new root,
+     *                as we still have to maintain the Min-Heap property.
+     * @return the re-balanced heap after replacing the root node with the new element.
+     */
+    public int[] replace(int elementValue) {
+        // calling deleteMinimum() & then insert() will re-balance the heap twice.
+        // So will replace the root with the new element, & "heapify" the array.
+        heap[0] = elementValue;
+
+        return buildHeap(heap);
+    }
+
+    /**
+     * Get the value in the root node, & delete it.
+     * Re-balances the heap after deleting the root node.
+     *
+     * @return the value in the root node.
+     */
+    public int extractMinimum() {
+        int minimum = findMinimum();
+        deleteMinimum();
+        return minimum;
+    }
+
+    /**
+     * Delete the root node & re-balance the heap.
+     *
+     * @return the re-balanced heap after removing the root node.
+     */
+    public int[] deleteMinimum() {
+        return delete(0);
+    }
+
+    /**
+     * Fetch the minimum value in the heap.
+     * For a MinHeap, it is the root node.
+     *
+     * @return the minimum value in the heap, i.e. the value in root node
+     */
+    public int findMinimum() {
+        return heap[0];
     }
 
     /**
