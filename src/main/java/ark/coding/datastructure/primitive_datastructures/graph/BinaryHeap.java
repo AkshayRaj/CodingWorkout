e46c6683bc67f666b8e21506d32e9205ad566c3f
akshayrajkore <akshayrajkore@gmail.com>
2019-10-29 00:06:51 -0400

    DataStructures: Implemented insert operation for MinHeap

---
+++ src/main/java/ark/coding/datastructure/primitive_datastructures/graph/BinaryHeap.java
@@ -0,0 +1,64 @@
+/**
+ * Created by Akshayraj
+ */
+package ark.coding.datastructure.primitive_datastructures.graph;
+
+import java.util.logging.Logger;
+
+public class BinaryHeap {
+
+    /**
+     * Returns the index of the parent of the element situated at {@code elementIndex}
+     *
+     * @param binaryHeap array representation of the binary heap
+     * @param elementIndex array index of the element
+     * @return index of the parent of the element.
+     */
+    protected int parent(int[] binaryHeap, int elementIndex) {
+        if (binaryHeap.length == 0) {
+            Logger.getGlobal().warning(String.format("Heap is not initialized. " +
+                    "Parent does not exist for element at index[%d]", elementIndex));
+            return -1;
+        }
+        if (elementIndex == 0) {
+            return 0;
+        }
+        int parent = (elementIndex - 1)/2;
+
+        return parent;
+    }
+
+    /**
+     * Returns the index to the left child of the current element of a {@link BinaryHeap}
+     *
+     * @param binaryHeap array representation of the binary heap
+     * @param elementIndex array index of the element
+     * @return index of the left child of the current element;
+     *         {@literal -1} if the element is a leaf node or left child does not exist.
+     */
+    protected int leftChild(int[] binaryHeap, int elementIndex) {
+        int leftChild = 2 * elementIndex + 1;
+        if (binaryHeap.length <= leftChild) {
+            return -1;
+        }
+
+        return leftChild;
+    }
+
+    /**
+     * Returns the index to the right child of the current element of a {@link BinaryHeap}
+     *
+     * @param binaryHeap array representation of the binary heap
+     * @param elementIndex array index of the element
+     * @return index of the right child of the current element;
+     *         {@literal -1} if the element is a leaf node or right child does not exist.
+     */
+    protected int rightChild(int[] binaryHeap, int elementIndex) {
+        int rightChild = 2 * elementIndex + 2;
+        if (binaryHeap.length <= rightChild) {
+            return -1;
+        }
+
+        return rightChild;
+    }
+}
--- src/main/java/ark/coding/datastructure/primitive_datastructures/graph/MinHeap.java
+++ src/main/java/ark/coding/datastructure/primitive_datastructures/graph/MinHeap.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
  * Thus, the maximum value of the heap is at the root, in case of a MaxHeap.
  * {@see https://www.educative.io/edpresso/min-heap-vs-max-heap}
  */
-public class MinHeap {
+public final class MinHeap extends BinaryHeap {
 
     /**
      * https://www.geeksforgeeks.org/array-representation-of-binary-heap/
@@ -36,6 +36,18 @@ public class MinHeap {
      */
     int[] heap = new int[]{};
 
+    /**
+     * An unsorted, unordered array of positive integers, that are arranged in a "heap" pattern within the array.
+     *
+     * After heapifying the array, the root element will be at Arr[0].
+     * Below table shows indexes of other nodes for the ith node, i.e., Arr[i]:
+     * - Arr[(i-1)/2]	Returns the parent node
+     * - Arr[(2*i)+1]	Returns the left child node
+     * - Arr[(2*i)+2]	Returns the right child node
+     *
+     * @param array to heapify.
+     * @return the heapified array.
+     */
     public int[] buildHeap(final int[] array) {
         // array representing the heap will have empty cells,
         // if the number of nodes in heap are not exactly equal to 2 raised to some power, i.e (2^power)
@@ -54,13 +66,66 @@ public class MinHeap {
         return heap;
     }
 
+    /**
+     * Merges the two heaps, represented by their corresponding array representations,
+     * into a single minimum binary heap.
+     *
+     * @param heap1 a minimum binary heap;
+     *              The root of heap1 is smaller than the root of heap2
+     * @param heap2 a minimum binary heap;
+     *              The root of heap1 is smaller than the root of heap2
+     * @return the merged heap.
+     */
+    public int[] mergeHeaps(int[] heap1, int[] heap2) {
+        if (heap1[0] > heap2[0]) {
+            return mergeHeaps(heap2, heap1);
+        }
+        // TODO: Implement merge of two min binary heaps
+
+        return heap;
+    }
+
+    /**
+     * Heapifies the subtree rooted at {@code index}.
+     *
+     * This assumes that the heap, except <i>only</i> the subtree rooted at {@code index}
+     * does not conform to the heap property, and hence the subtree rooted at {@code index}
+     * needs to be heapified.
+     *
+     * <b>Note: To heapify the entire array, use {@link #buildHeap(int[])}</b>
+     *
+     * @param heap the heap, whose one & only one subtree rooted at a given index does not obey the heap property.
+     * @param index of the subtree which does not conform to the heap property.
+     */
     private void heapify(final int[] heap, int index) {
         // TODO: Implement the heapify algorithm.
     }
 
-    public int insert(final int element) {
+    /**
+     * Insert an element with the given value.
+     *
+     * @param elementValue
+     * @return the array index, where the new element is stored.
+     */
+    public int insert(final int elementValue) {
         int elementKey = -1;
-        // TODO: Insert the element, and re-balance the heap
+
+        int newLengthOfHeap = heap.length + 1;
+        int newElementIndex = newLengthOfHeap - 1; // place the new element at the end of the heap (or array)
+        int[] tmpHeap = Arrays.copyOf(heap, newLengthOfHeap);
+        tmpHeap[newElementIndex] = elementValue; // add the new element in the last location
+
+        // pull-up if value of new element is less than the parent
+        int parentIndex = parent(tmpHeap, newElementIndex);
+        while (tmpHeap[parentIndex] > tmpHeap[newElementIndex]) {
+            swap(tmpHeap, parentIndex, newElementIndex);
+            newElementIndex = parentIndex;
+            parentIndex = parent(tmpHeap, newElementIndex);
+        }
+        // ^^^ while loop takes O(log(n)) operations
+        // to insert a new element & re-balance the heap.
+        heap = tmpHeap;
+        elementKey = newElementIndex;
 
         return elementKey;
     }
@@ -72,13 +137,19 @@ public class MinHeap {
      * @param newValue the new value of the element
      * @return the new key (array index) of the element.
      */
-    public int decreaseKey(final int elementKey, final int newValue) {
+    public int decreaseElement(final int elementKey, final int newValue) {
         int newKey = -1;
         // TODO: Decreases value of element to newValue
 
         return newKey;
     }
 
+    /**
+     * Delete an element at a given index.
+     *
+     * @param elementKey the key (or the index in array) of the element
+     * @return the re-balanced heap, after deleting the element.
+     */
     public int[] delete(final int elementKey) {
         // TODO: re-balance the heap after deleting the element from the heap.
 
@@ -86,20 +157,24 @@ public class MinHeap {
     }
 
     /**
-     * Pop the root, and push the new element.
-     * Re-balances the heap after pop & push.
+     * Replaces the root element with the given element.
+     * This does not mean the given element becomes the new root; rather we are replacing the element at root node
+     * with the new element.
      *
-     * @param elementValue the element to replace the root.
+     * This pops the root, and pushes the new element. Re-balances the heap after pop & push.
+     *
+     * @param newElementValue the element to replace the root.
      *                Note that, the new element may not become the new root,
      *                as we still have to maintain the Min-Heap property.
      * @return the re-balanced heap after replacing the root node with the new element.
      */
-    public int[] replace(int elementValue) {
+    public int[] replace(int newElementValue) {
         // calling deleteMinimum() & then insert() will re-balance the heap twice.
         // So will replace the root with the new element, & "heapify" the array.
-        heap[0] = elementValue;
+        heap[0] = newElementValue;
 
-        return buildHeap(heap);
+        heapify(heap, 0);
+        return heap;
     }
 
     /**
@@ -143,5 +218,12 @@ public class MinHeap {
             return new int[]{};
         }
         return heap;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int valueAtIndex1 = array[index1];
+        int valueAtIndex2 = array[index2];
+        array[index1] = valueAtIndex2;
+        array[index2] = valueAtIndex1;
     }
 }
