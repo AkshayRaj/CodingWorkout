2944a31849d971367d76d93fda6990b5d8b1938d
akshayrajkore <akshayrajkore@gmail.com>
2020-07-14 14:38:42 -0400

    IK: Documentation update for partitioning methods

--- src/main/java/ark/coding/books/interview_kickstart/IKSort.java
+++ src/main/java/ark/coding/books/interview_kickstart/IKSort.java
@@ -54,68 +54,76 @@ public class IKSort {
     }
 
     private static void quickSort(int[] array) {
-        hoaresPartition(array, 0, array.length-1);
+        //lomutosPartition(array, 0, array.length-1);
+        //hoaresPartition(array, 0, array.length-1);
     }
 
-    private static void lomutosPartition(int[] array, int low, int high) {
+    private static void lomutosPartition(int[] subArray, int low, int high) {
         if (low >= high) {
             return;
         }
-        int pivot = ((int)Math.random()%(high-low+1)) + low;
-        swapElements(array, pivot, low); // store pivot at lowIndex
+        // Place the pivot at the start of the sub-array.
+        int pivotIdx = ((int)(Math.random()*Integer.MAX_VALUE)%(high-low+1)) + low;
+        swapElements(subArray, low, pivotIdx);
+        int pivot = subArray[low];
 
-        int index = low+1; // iterates from low+1 to end of array
-        int partitionIndex = low; // pointer keeps track of elements smaller than pivot
-        while (index <= high) {
-            if (array[index] <= array[low]) {
-                partitionIndex++;
-                swapElements(array, index, partitionIndex);
+        // Lomuto's Partition:
+        // Two pointers start from same end, but in successive positions & go in same direction at different speeds.
+        // 1. The first pointer iterates over the array - so its faster
+        // 2. The second pointer is used to keep track of elements smaller than pivot.
+        //    All elements to left of this pointer are smaller than pivot
+        //    Thus, the second pointer is slower.
+        int smallerIdx = low; // keeps track of rightest element less than pivot
+        for (int index = low+1; index <= high; index++) {
+            if (subArray[index] < pivot) {
+                smallerIdx++;
+                swapElements(subArray, index, smallerIdx);
             }
-            index++;
         }
-        swapElements(array, low, partitionIndex);
+        // at the end of `partitionLoop`, smallerIdx will be pointing to the partitionIdx.
+        // Partition Index is the index that partitions the array into two parts.
+        int partitionIdx = smallerIdx;
+        swapElements(subArray, low, partitionIdx);
 
         // sort left & right sub-arrays
-        lomutosPartition(array, low, partitionIndex-1);
-        lomutosPartition(array, partitionIndex+1, high);
+        lomutosPartition(subArray, low, smallerIdx-1);
+        lomutosPartition(subArray, smallerIdx+1, high);
     }
 
-    private static void hoaresPartition(int[] array, int low, int high) {
+    private static void hoaresPartition(int[] subArray, int low, int high) {
         if (low >= high) {
             return;
         }
 
         // Place the pivot to the start of the sub-array.
         int pivotIdx = ((int)(Math.random()*Integer.MAX_VALUE)%(high-low+1)) + low;
-        swapElements(array, low, pivotIdx);
-        int pivot = array[low];
+        swapElements(subArray, low, pivotIdx);
+        int pivot = subArray[low];
 
         // Hoare's Partition :
-        // Two pointers go in opposite direction.
+        // Two pointers go in opposite direction, and start from opposite ends towards each other.
         // Pointers stop after they "cross" each other (not when they meet, they have to cross)
         int leftIndex = low+1; // ~>> left to right
         int rightIndex = high; // <<~ right to left
         partitionLoop: while (leftIndex <= rightIndex) {
-            checkForBiggerNumber:  while (leftIndex <= high && array[leftIndex] < pivot)
+            checkForBiggerNumber:  while (leftIndex <= high && subArray[leftIndex] < pivot)
                                             leftIndex++;
-            checkForSmallerNumber: while (rightIndex > low && array[rightIndex] >= pivot)
+            checkForSmallerNumber: while (rightIndex > low && subArray[rightIndex] >= pivot)
                                             rightIndex--;
 
             // swap elements in wrong positions.
             // At this point leftIdx & rightIdx _could_ have crossed each other. So check for that.
             if (leftIndex < rightIndex) {
-                swapElements(array, leftIndex, rightIndex);
+                swapElements(subArray, leftIndex, rightIndex);
             }
         }
-
         // at the end of `partitionLoop`, rightIdx will be pointing to the partitionIdx.
         // Partition Index is the index that partitions the array into two parts.
         int partitionIndex = rightIndex;
-        swapElements(array, low, partitionIndex);
+        swapElements(subArray, low, partitionIndex);
 
         // sort left & right sub-arrays
-        hoaresPartition(array, low, partitionIndex-1);
-        hoaresPartition(array, partitionIndex+1, high);
+        hoaresPartition(subArray, low, partitionIndex-1);
+        hoaresPartition(subArray, partitionIndex+1, high);
     }
-
 }
