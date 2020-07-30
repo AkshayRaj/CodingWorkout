e043c6adbce7c13a09194a4ca439660107ec9d83
Akshayraj Kore <akshayrajkore@gmail.com>
2020-07-30 01:28:10 -0400

    IK: Simulate pre & post order recursion using stack

--- src/main/java/ark/coding/books/interview_kickstart/trees/dfs/iterative/BinaryTreePreOrder.java
+++ src/main/java/ark/coding/books/interview_kickstart/trees/dfs/iterative/BinaryTreePreOrder.java
@@ -4,6 +4,7 @@
 package ark.coding.books.interview_kickstart.trees.dfs.iterative;
 
 import ark.coding.books.interview_kickstart.trees.TreeNode;
+import ark.coding.tools.Utils;
 
 import java.util.ArrayList;
 import java.util.Deque;
@@ -27,6 +28,25 @@ public class BinaryTreePreOrder {
             if (head.left != null) stack.push(head.left);
         }
 
+        return solution;
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> solution = new ArrayList<>();
+        if (root == null) return solution;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode head = stack.pop();
+            solution.add(head.val);
+
+            if (head.left != null) stack.push(head.left);
+            if (head.right != null) stack.push(head.right);
+        }
+
+        Utils.reverseList(solution);
         return solution;
     }
 }
