3cba2f01bac782d658aaa1fd66bb07eb7d3f20ba
Akshayraj Kore <akshayrajkore@gmail.com>
2020-10-10 19:47:57 -0400

    IK: practice bit manipulation

--- src/main/java/ark/coding/books/interview_kickstart/bit_manipulation/TwosComplement.java
+++ src/main/java/ark/coding/books/interview_kickstart/bit_manipulation/TwosComplement.java
@@ -1,4 +1,18 @@
 package ark.coding.books.interview_kickstart.bit_manipulation;
 
 public class TwosComplement {
+
+    /**
+     * https://leetcode.com/problems/power-of-two
+     */
+    public boolean isPowerOfTwo(int n) {
+        return false;
+    }
+
+    /**
+     * https://leetcode.com/problems/single-number-iii/solution/
+     */
+    public int[] singleNumber(int[] nums) {
+        return null;
+    }
 }
