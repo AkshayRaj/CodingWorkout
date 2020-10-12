311677c8015702c7c80f35d42dbaf5f65de50363
Akshayraj Kore <akshayrajkore@gmail.com>
2020-10-11 21:20:43 -0400

    IK: Track path by using parent node (keys&doors) [GRAPH]

--- src/main/java/ark/coding/books/interview_kickstart/graphs/KeysAndDoorsShortestPath.java
+++ src/main/java/ark/coding/books/interview_kickstart/graphs/KeysAndDoorsShortestPath.java
@@ -30,14 +30,20 @@ public class KeysAndDoorsShortestPath {
         //    - track cells visited with same keyset
         boolean[][][] visited = new boolean[grid.length][grid[0].length()][1024];
         Queue<Node> frontier = new LinkedList<>();
-        frontier.add(new Node(start[0],start[1], grid, new ArrayList<>(), 0));
+        frontier.add(new Node(start[0],start[1], grid, null, 0));
         visited[start[0]][start[1]][0] = true;
         while (!frontier.isEmpty()) {
             Node head = frontier.poll();
 
             if (isFinalNode(head)) {
-                solution = new int[head.path.size()][2];
-                for (int idx = 0; idx < head.path.size(); idx++) solution[idx] = head.path.get(idx);
+                List<int[]> path = new ArrayList<>();
+                while (head != null) {
+                    path.add(new int[]{head.row, head.col});
+                    head = head.parent;
+                }
+
+                solution = new int[path.size()][2];
+                for (int idx = path.size()-1; idx >= 0; idx--) solution[path.size()-1-idx] = path.get(idx);
                 return solution;
             }
 
@@ -52,22 +58,22 @@ public class KeysAndDoorsShortestPath {
 
         // up
         if (head.row-1 >= 0) {
-            Node neighbour = new Node(head.row-1, head.col, grid, head.path, head.keyset);
+            Node neighbour = new Node(head.row-1, head.col, grid, head, head.keyset);
             addNeighbourIfNotVisitedWithKeySuperset(neighbour, neighbours, visited);
         }
         // down
         if (head.row+1 < grid.length) {
-            Node neighbour = new Node(head.row+1, head.col, grid, head.path, head.keyset);
+            Node neighbour = new Node(head.row+1, head.col, grid, head, head.keyset);
             addNeighbourIfNotVisitedWithKeySuperset(neighbour, neighbours, visited);
         }
         // left
         if (head.col-1 >= 0) {
-            Node neighbour = new Node(head.row, head.col-1, grid, head.path, head.keyset);
+            Node neighbour = new Node(head.row, head.col-1, grid, head, head.keyset);
             addNeighbourIfNotVisitedWithKeySuperset(neighbour, neighbours, visited);
         }
         // right
         if (head.col+1 < grid[head.row].length()) {
-            Node neighbour = new Node(head.row, head.col+1, grid, head.path, head.keyset);
+            Node neighbour = new Node(head.row, head.col+1, grid, head, head.keyset);
             addNeighbourIfNotVisitedWithKeySuperset(neighbour, neighbours, visited);
         }
 
@@ -138,21 +144,20 @@ public class KeysAndDoorsShortestPath {
         final int row;
         final int col;
         final char datum;
-        final List<int[]> path;
+        final Node parent;
         final int keyset; //bitmask 0 ~> 1023 (2^10); there are 10 keys from 'a' ~> 'j'
 
-        Node(int row, int col, String[] grid, List<int[]> path, int keyset) {
+        Node(int row, int col, String[] grid, Node parent, int neighboursKeyset) {
             this.row = row;
             this.col = col;
             this.datum = grid[row].charAt(col);
-            this.path = new ArrayList<>(path);
-            this.path.add(new int[]{row, col});
+            this.parent = parent;
             if(isKey(datum)) {
                 // add key to keyset
-                this.keyset = keyset | KEY_SET[datum - 'a'];
+                this.keyset = neighboursKeyset | KEY_SET[datum - 'a'];
             }
             else {
-                this.keyset = keyset;
+                this.keyset = neighboursKeyset;
             }
         }
 
