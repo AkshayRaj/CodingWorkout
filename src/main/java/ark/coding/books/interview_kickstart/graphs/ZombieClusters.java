2f39f44c0b481c5a8450a87c8f9efd91e2d6ceec
Akshayraj Kore <akshayrajkore@gmail.com>
2020-10-17 14:59:00 -0400

    IK: [Graphs] find number of clusters using DFS/stack

--- src/main/java/ark/coding/books/interview_kickstart/graphs/ZombieClusters.java
+++ src/main/java/ark/coding/books/interview_kickstart/graphs/ZombieClusters.java
@@ -1,39 +1,35 @@
 package ark.coding.books.interview_kickstart.graphs;
 
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://uplevel.interviewkickstart.com/resource/rc-codingproblem-1705-9596-54-335
  */
 public class ZombieClusters {
-    static boolean[][] visited;
-
     public static int zombieCluster(List<String> zombies) {
-        int size = zombies.size();
+        int noOfNodes = zombies.size();
 
         int clusters = 0;
-        visited = new boolean[size][size];
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                if (zombies.get(row).charAt(col) == '1' && !visited[row][col]) {
-                    dfs(zombies, row, col);
-                    clusters++;
+        boolean[] visited = new boolean[noOfNodes];
+        Stack<Integer> stack = new Stack<>();
+        for (int node = 0; node < noOfNodes; node++) {
+            if (!visited[node]) {
+                clusters++;
+
+                // DFS and traverse the graph
+                stack.push(node);
+                while (!stack.isEmpty()) {
+                    Integer currentNode = stack.pop();
+                    visited[currentNode] = true;
+                    for (int neighbour = 0; neighbour < noOfNodes; neighbour++) {
+                        if (zombies.get(currentNode).charAt(neighbour) == '1' && !visited[neighbour]) {
+                            stack.push(neighbour);
+                        }
+                    }
                 }
             }
         }
-
         return clusters;
-    }
-
-    static void dfs(List<String> zombies, int row, int col) {
-        if (visited[row][col]) return;
-
-        visited[row][col] = true;
-        visited[col][row] = true;
-        for (int neighbour = 0; neighbour < zombies.size(); neighbour++) {
-            if (zombies.get(col).charAt(neighbour) == '1') {
-                dfs(zombies, col, neighbour);
-            }
-        }
     }
 }
